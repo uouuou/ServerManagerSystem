98b99619cc84bf91aef3b1b0873604f4899a1482
Rinvay.H <hrinvay@gmail.com>
2021-07-17 08:46:33 +0800

    更新构建脚本

--- build.sh
+++ build.sh
@@ -6,7 +6,6 @@ PROJECT_NAME="ServerManagerSystem"
 BINARY="main"
 Version=$(date "+%Y%m%d%H%M%S")
 OUTPUT_DIR=output
-GOOS=$(go env GOOS)
 
 APP_NAME=${PROJECT_NAME}
 APP_VERSION=SM${Version}
@@ -15,30 +14,8 @@ BUILD_TIME=$(date "+%FT%T%z")
 GIT_REVISION=$(git rev-parse --short HEAD)
 GIT_BRANCH=$(git name-rev --name-only HEAD)
 GO_VERSION=$(go version)
+SYS_VERSION=$(uname -m)
 
-# 构建一个ARM V8版本由于使用了SQLLite需要引入一个gcc编译器
-GOARCH=arm64 GOOS=linux CGO_ENABLED=1 CC=/usr/lib/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-gcc-10.2.1 go build -a -installsuffix cgo -v -mod=vendor \
-  -ldflags "-s -w -X 'github.com/uouuou/ServerManagerSystem/models.AppName=${APP_NAME}' \
-            -X 'github.com/uouuou/ServerManagerSystem/models.AppVersion=${APP_VERSION}' \
-            -X 'github.com/uouuou/ServerManagerSystem/models.BuildVersion=${BUILD_VERSION}' \
-            -X 'github.com/uouuou/ServerManagerSystem/models.BuildTime=${BUILD_TIME}' \
-            -X 'github.com/uouuou/ServerManagerSystem/models.GitRevision=${GIT_REVISION}' \
-            -X 'github.com/uouuou/ServerManagerSystem/models.GitBranch=${GIT_BRANCH}' \
-            -X 'github.com/uouuou/ServerManagerSystem/models.GoVersion=${GO_VERSION}'" \
-  -o ${OUTPUT_DIR}/${PROJECT_NAME}_linux_armv8 ${BINARY}.go
-
-# 构建一个x86_64版本由于sqllite的原因需要使用CGO_ENABLED=1
-#CGO_ENABLED=1 GOOS=windows GOARCH=amd64 CC=/usr/bin/x86_64-w64-mingw32-gcc-win32 go build -a -installsuffix cgo -v -mod=vendor \
-#  -ldflags "-s -w -X 'github.com/uouuou/ServerManagerSystem/models.AppName=${APP_NAME}' \
-#            -X 'github.com/uouuou/ServerManagerSystem/models.AppVersion=${APP_VERSION}' \
-#            -X 'github.com/uouuou/ServerManagerSystem/models.BuildVersion=${BUILD_VERSION}' \
-#            -X 'github.com/uouuou/ServerManagerSystem/models.BuildTime=${BUILD_TIME}' \
-#            -X 'github.com/uouuou/ServerManagerSystem/models.GitRevision=${GIT_REVISION}' \
-#            -X 'github.com/uouuou/ServerManagerSystem/models.GitBranch=${GIT_BRANCH}' \
-#            -X 'github.com/uouuou/ServerManagerSystem/models.GoVersion=${GO_VERSION}'" \
-#  -o ${OUTPUT_DIR}/${PROJECT_NAME}_windows_x86_64 ${BINARY}.go
-
-# 构建一个amd64版本由于sqllite的原因需要使用CGO_ENABLED=1
 CGO_ENABLED=1 go build -a -installsuffix cgo -v -mod=vendor \
   -ldflags "-s -w -X 'github.com/uouuou/ServerManagerSystem/models.AppName=${APP_NAME}' \
             -X 'github.com/uouuou/ServerManagerSystem/models.AppVersion=${APP_VERSION}' \
@@ -47,13 +24,11 @@ CGO_ENABLED=1 go build -a -installsuffix cgo -v -mod=vendor \
             -X 'github.com/uouuou/ServerManagerSystem/models.GitRevision=${GIT_REVISION}' \
             -X 'github.com/uouuou/ServerManagerSystem/models.GitBranch=${GIT_BRANCH}' \
             -X 'github.com/uouuou/ServerManagerSystem/models.GoVersion=${GO_VERSION}'" \
-  -o ${OUTPUT_DIR}/${PROJECT_NAME}_linux_amd64 ${BINARY}.go
+  -o ${OUTPUT_DIR}/${PROJECT_NAME}_${SYS_VERSION} ${BINARY}.go
 
 #删除原有的UPX压缩包并创建新的压缩包
-rm -rf ${OUTPUT_DIR}/${PROJECT_NAME}_linux_armv8_upx
-rm -rf ${OUTPUT_DIR}/${PROJECT_NAME}_linux_amd64_upx
-upx -5 -o ${OUTPUT_DIR}/${PROJECT_NAME}_linux_armv8_upx ${OUTPUT_DIR}/${PROJECT_NAME}_linux_armv8
-upx -5 -o ${OUTPUT_DIR}/${PROJECT_NAME}_linux_amd64_upx ${OUTPUT_DIR}/${PROJECT_NAME}_linux_amd64
+rm -rf ${OUTPUT_DIR}/${PROJECT_NAME}_${SYS_VERSION}_upx
+upx -5 -o ${OUTPUT_DIR}/${PROJECT_NAME}_${SYS_VERSION}_upx ${OUTPUT_DIR}/${PROJECT_NAME}_${SYS_VERSION}
 
 # 执行结束后打印版本号
 echo "--------------------------"
