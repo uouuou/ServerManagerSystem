9312ee47dfdc2c0691b36481dba85183527e9515
Rinvay.H <hrinvay@gmail.com>
2022-05-26 19:58:25 +0800

    更新分页算法

--- middleware/pages.go
+++ middleware/pages.go
@@ -11,16 +11,25 @@ func GetPages(db *gorm.DB, pageNow string, pageSizeNow string, value interface{}
 	page, _ := strconv.Atoi(pageNow)
 	pageSize, _ := strconv.Atoi(pageSizeNow)
 	Db := db.Model(value)
-	pages.TotalAmount = Db.Where("deleted_at IS NULL").Find(value).RowsAffected
+	Db.Count(&pages.TotalAmount)
 	if page > 0 && pageSize > 0 {
 		Db.Limit(pageSize).Offset((page - 1) * pageSize)
 		pages.Page = page
 		pages.PageSize = pageSize
-
-	} else if pageSize == -1 {
+	}
+	if page > 0 && pageSize == 0 {
+		Db.Limit(pageSize).Offset((page - 1) * pageSize)
 		pages.Page = page
+		pages.PageSize = 15
+	}
+	if page == 0 && pageSize > 0 {
+		Db.Limit(pageSize)
+		pages.Page = 1
 		pages.PageSize = pageSize
-	} else {
+	}
+	if page == 0 && pageSize == 0 {
+		pages.Page = 1
+		pages.PageSize = 15
 		Db = Db.Limit(15)
 	}
 	return pages, Db
